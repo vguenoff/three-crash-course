1e455ca112d38146b7ea9d6f06d74946a126102d
Vasil Genov <vasil.genov@psinteractive.io>
2023-03-31 22:44:23 +0300

    resize window func

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import * as THREE from 'three'
 import './style.css'
 
+// Sizes
+const useWindowDimensions = () => [window.innerWidth, window.innerHeight]
+let [w, h] = useWindowDimensions()
+
 // Scene
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 100)
 
 // Renderer
 const canvas = document.querySelector('.webgl') as HTMLCanvasElement
 const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(w, h)
 
 // Sphere
 const geometry = new THREE.SphereGeometry(1, 64, 64)
@@ -25,3 +29,19 @@ scene.add(light)
 camera.position.z = 10
 
 renderer.render(scene, camera)
+
+// Resize
+window.addEventListener('resize', () => {
+  ;[w, h] = useWindowDimensions()
+  camera.aspect = w / h
+  camera.updateProjectionMatrix()
+  renderer.setSize(w, h)
+})
+
+// Loop
+function loop() {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+
+loop()
