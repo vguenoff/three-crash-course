1969b5d3a299e9682e02a77dcb2c7f3877cbe156
Vasil Genov <vasil.genov@psinteractive.io>
2023-03-31 18:32:02 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,25 @@ import './style.css'
 
 // Scene
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100)
 
 // Renderer
-const renderer = new THREE.WebGLRenderer()
+const canvas = document.querySelector('.webgl') as HTMLCanvasElement
+const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
 
 // Sphere
-const geometry = new THREE.SphereGeometry(1, 64, 64, 0, 1.2)
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+const geometry = new THREE.SphereGeometry(1, 64, 64)
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff83 })
 const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
 
+// Light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
 // View
-camera.position.z = 5
+camera.position.z = 10
 
-function animate() {
-  requestAnimationFrame(animate)
-
-  sphere.rotation.x += 0.01
-  sphere.rotation.y += 0.01
-
-  renderer.render(scene, camera)
-}
-
-animate()
+renderer.render(scene, camera)
