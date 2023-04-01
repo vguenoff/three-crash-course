920c71d23d199ec847de203e3da78d960ce7b43a
Vasil Genov <vasil.genov@psinteractive.io>
2023-04-01 16:43:09 +0300

    back to green ball

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import './style.css'
 const useWindowDimensions = () => [window.innerWidth, window.innerHeight]
 let [w, h] = useWindowDimensions()
 
-// Clock
-const clock = new THREE.Clock()
-
 // Scene
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 100)
@@ -19,13 +16,13 @@ renderer.setSize(w, h)
 
 // Sphere
 const geometry = new THREE.SphereGeometry(1, 64, 64)
-const material = new THREE.MeshStandardMaterial({ color: 0xf6f1d5 })
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff83 })
 const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
 
 // Light
 const light = new THREE.PointLight(0xffffff, 1.2, 100)
-// light.position.set(10, 10, 10)
+light.position.set(0, 10, 10)
 scene.add(light)
 
 // View
@@ -43,20 +40,8 @@ window.addEventListener('resize', () => {
 
 // Loop
 function loop() {
-  animate(clock.getElapsedTime())
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
-}
-
-function animate(elapsedTime: number) {
-  sphere.rotation.y = elapsedTime
-  light.position.set(
-    Math.sin(elapsedTime) * Math.PI * 2,
-    Math.sin(elapsedTime) * Math.PI * 2,
-    Math.cos(elapsedTime) * Math.PI * 2
-  )
-  sphere.position.y = Math.sin(elapsedTime)
-  sphere.position.x = Math.cos(elapsedTime)
 }
 
 loop()
