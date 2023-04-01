6887d58c0c38e1326b6b6a686cac701bb719e9e3
Vasil Genov <vasil.genov@psinteractive.io>
2023-04-01 18:10:53 +0300

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import './style.css'
+import gsap from 'gsap'
+
+// Show
+document.body.style.display = 'block'
 
 // Sizes
 const useWindowDimensions = () => [window.innerWidth, window.innerHeight]
@@ -17,13 +21,14 @@ renderer.setSize(w, h)
 
 // Sphere
 const geometry = new THREE.SphereGeometry(1, 64, 64)
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff83 })
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff83, roughness: 0.7 })
 const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
 
 // Light
 const light = new THREE.PointLight(0xffffff, 1.2, 100)
 light.position.set(5, 10, 10)
+light.intensity = 1.25
 scene.add(light)
 
 // Controls
@@ -34,11 +39,29 @@ controls.enableZoom = false
 controls.autoRotate = true
 controls.autoRotateSpeed = 3
 
+// Mouse animation colors
+let mouseDown = false
+window.addEventListener('mousedown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+window.addEventListener('mousemove', e => {
+  if (mouseDown) {
+    const rgb = [Math.round((e.pageX / w) * 255), Math.round((e.pageY / h) * 255), Math.round(Math.random() * 255)]
+    gsap.to(sphere.material.color, new THREE.Color(`rgb(${rgb.join(',')})`))
+  }
+})
+
 // View
-camera.position.z = 7
+camera.position.z = 10
 
 renderer.setPixelRatio(2)
 renderer.render(scene, camera)
+
+// GSAP
+const tl = gsap.timeline({ defaults: { duration: 1, ease: 'ease-in-out' } })
+// tl.fromTo('body', { opacity: 0 }, { opacity: 1 }, 0)
+tl.fromTo(sphere.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 })
+tl.fromTo('nav', { y: '-100%' }, { y: 0 })
+tl.fromTo('h1', { opacity: 0 }, { opacity: 1 })
 
 // Resize
 window.addEventListener('resize', () => {
